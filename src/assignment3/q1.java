85f4c2d890df366333a5c95c5c4533c63ad98414
tpiggo <piggott291@gmail.com>
2021-03-24 19:29:51 -0400

    Adding work done for q1. No speedup yet. Working toward that through convergence. Converging all the speculative threads in order to not have to compute 5 iterations of the same thing.

--- src/assignment3/q1.java
+++ src/assignment3/q1.java
@@ -3,7 +3,8 @@ package assignment3;
  * NOTE: Always clean the directory out from the stuff you added 
  * Always run rmdir -f assignment3 before committing
  */
-
+import java.io.FileWriter;
+import java.nio.channels.AcceptPendingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -11,6 +12,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import javax.sound.sampled.AudioFormat.Encoding;
+
 
 class Transition {
     private Set<Character> characters;
@@ -82,14 +92,11 @@ class DFAGraph {
     State startState;
     ArrayList<State> states;
     State acceptState;
-    Queue<Character> charQueue;
-    StringBuilder finalString;
     public DFAGraph() {
         /**
          * TODO: Make this less ugly. This set up could be improved?
          */
         this.states = new ArrayList<>();
-        charQueue = new LinkedList<>();
         // Hardcoded state creation since every state is different than the next
         State acceptNode = new State(5, Transition.fullCTransition());
         this.states.add(acceptNode);
@@ -108,7 +115,6 @@ class DFAGraph {
             );
         this.startState = start;
         this.states.add(start);
-        finalString = new StringBuilder();
     }
 
     public State getState(int id) {
@@ -121,22 +127,266 @@ class DFAGraph {
             if (aState.id == id)
                 return aState;
         }
+
         return null;
-    } 
+    }
+}
+
+/**
+ * Object for storing the simulation of the DFA
+ */
+class EncodingType{
+    State start;
+    State end;
+    StringBuilder aString;
+    int pos = 0;
+    public EncodingType(State pStart, State pEnd, String pString) {
+        this.start = pStart;
+        this.end = pEnd;
+        this.aString = new StringBuilder(pString);
+    }
+}
+
+class DFAReader{
+    private ExecutorService executor;
+    private DFAGraph dfa;
+    int id;
+    int taskLength;
     
-    public String runOn(String aString) {
+    DFAReader(DFAGraph pDfa, int n){
+        this.executor = Executors.newFixedThreadPool(n);
+        this.dfa = pDfa;
+        this.id = 1;
+    }
+    /**
+     * get the list of the simulations
+     * @return List of encoded types.
+     */
+    public Future<EncodingType[]> simulDFA(String aString) {
+        return executor.submit(
+            new DFATask(this.dfa, aString, ++this.id)
+            );
+    }
+
+    /**
+     * get the list of the simulations
+     * @return List of encoded types.
+     */
+    public Future<EncodingType[]> simulDFA(DFATask aTask) { 
+        return executor.submit(aTask);
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+    }
+
+}
+
+class DFATask implements Callable<EncodingType[]> {
+    Queue<Character> charQueue;
+    DFAGraph dfa;
+    String aString;
+    int id;
+    int maxStartState = 5;
+    EncodingType[] aEncoding =  new EncodingType[5];
+    
+    public DFATask(DFAGraph pDFA, String inputString, int pId) {
+        charQueue = new LinkedList<>();
+        dfa = pDFA;
+        aString = inputString;
+        id = pId;
+        // Create empty encodings
+        for (int i = 0; i < aEncoding.length; i++){
+            aEncoding[i] = new EncodingType(dfa.getState(i+1), dfa.getState(i+1), "");
+        }
+    }
+
+    private void pushEndState(State currentState, EncodingType eType) {
+        if (currentState != null && currentState.equals(dfa.getState(5))) {
+            while (!charQueue.isEmpty()) {
+                eType.aString.append(charQueue.poll());
+            }
+        } else if (currentState != null && currentState != dfa.getState(5)) {
+            while (!charQueue.isEmpty()) {
+                charQueue.poll();
+                eType.aString.append("_");
+            }
+        }
+    }
+
+    private EncodingType simulateDFA(int startPos, State startState) {
         State prevState = null;
-        State currentState = this.startState;
+        State currentState = startState;
+        StringBuilder fBuilder = new StringBuilder();
+        // Main body. Read the string char by char and perform the necessary computations
+        for (int i = startPos; i < aString.length(); i++) {
+            prevState = currentState;
+            currentState = currentState.readChar(aString.charAt(i));
+            if (currentState == null && prevState.equals(dfa.getState(5))) {
+                while (!charQueue.isEmpty()) {
+                    fBuilder.append(charQueue.poll());
+                }
+                fBuilder.append("_");
+                currentState = dfa.getState(1);
+            } else if (currentState == null && !prevState.equals(dfa.getState(5))) {
+                while (!charQueue.isEmpty()) {
+                    charQueue.poll();
+                    fBuilder.append("_");
+                }
+                if (aString.charAt(i) > 48 && aString.charAt(i) < 57)
+                    i--;                   
+                else 
+                    fBuilder.append("_");
+                currentState = dfa.getState(1);
+            } else {
+                charQueue.add(aString.charAt(i));
+            }
+        }
+        EncodingType aEncoding = new EncodingType(startState, currentState, fBuilder.toString());
+        pushEndState(currentState, aEncoding);
+        
+        return aEncoding;
+    }
+
+    private boolean readToNextBreak(EncodingType pEncoding, int id){
+        State prevState = null;
+        State currentState = pEncoding.end;
+        StringBuilder fBuilder = new StringBuilder();
+        int pos = pEncoding.pos;
+        for (; pos < aString.length(); pos++) {
+            prevState = currentState;
+            currentState = currentState.readChar(aString.charAt(pos));
+            if (currentState == null) {
+                if (prevState.equals(dfa.getState(5))) {
+                    while (!charQueue.isEmpty()) {
+                        fBuilder.append(charQueue.poll());
+                    }
+                    fBuilder.append("_");
+                } else {
+                    while (!charQueue.isEmpty()) {
+                        charQueue.poll();
+                        fBuilder.append("_");
+                    }
+    
+                    if (aString.charAt(pos) > 48 && aString.charAt(pos) < 57)
+                        pos--;
+                    else 
+                    fBuilder.append("_");
+                }
+                if (pos < 0) {
+                    pos = 0;
+                }
+
+                pos++;
+                pEncoding.end = dfa.getState(1);
+                break;
+            } else {
+                charQueue.add(aString.charAt(pos));
+            }
+        }
+        
+        for (int i = 0; i < aEncoding.length; i++) {
+            if (i != id && pEncoding.pos != 0 && aEncoding[i].pos == pEncoding.pos && aEncoding[i].end == pEncoding.end ) {
+                // This element was at the same place!
+                aEncoding[i].aString.append(fBuilder.toString());
+                aEncoding[i].pos = pos;
+            }
+        }
+
+        pEncoding.aString.append(fBuilder.toString());
+        pEncoding.pos = pos;
+        // Reached the end of the string :0
+        if (pos >= aString.length()){
+            if (charQueue.isEmpty())
+                pushEndState(currentState, pEncoding);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public EncodingType[] call() throws Exception {
+        // Read only small bits until ALL machines converge, and read it once.
+        int j = 0;
+        int d = 0;
+        while (d < 5) {
+            if (readToNextBreak(aEncoding[j], j)) {
+                d++;
+            }
+            if (aEncoding[j].aString.length() >= 49){
+                System.out.println("here " + aEncoding[j].aString.length());
+            }
+            j = (j+1)%5;
+        }
+
+        EncodingType finalEncoding = simulateDFA(aEncoding[0].pos, aEncoding[0].end);
+
+        for (int i = 0; i < aEncoding.length; i++) {
+            aEncoding[i].aString.append(finalEncoding.aString.toString());
+            aEncoding[i].end = finalEncoding.end;
+            aEncoding[i].pos = finalEncoding.pos;
+        }
+
+        return aEncoding;
+    }
+}
+
+class NormalRunnable implements Runnable {
+    private String aString;
+    private State endState = null;
+    private DFAReader aReader = null;
+    private StringBuilder finalString;
+    private ArrayList<Future<EncodingType[]>> aFutures;
+    private DFATask[] subtasks;
+    private DFAGraph aDFA;
+    private Queue<Character> charQueue;
+
+
+    public NormalRunnable(int oThreads, String pString, DFAGraph dfa) {
+        int partition = pString.length()/(oThreads+1);
+        
+        if (pString.length()%(oThreads+1) != 0) {
+            partition++;
+        }
+        
+        aString = pString.substring(0, partition);
+        
+        charQueue = new LinkedList<>();
+        aFutures = new ArrayList<>();
+        subtasks = new DFATask[oThreads];
+        
+        for (int i = 1; i < oThreads+1; i++) {
+            int strLen = partition*(i+1)>pString.length()?partition*(i+1):pString.length();
+            subtasks[i-1] = new DFATask(dfa, pString.substring((i)*partition, strLen), i);
+        }
+
+        if (oThreads != 0)
+            aReader = new DFAReader(dfa, oThreads);
+        aDFA = dfa;
+        finalString = new StringBuilder();
+    }
+
+    @Override
+    public void run() {
+        // Start all the sub tasks
+        for (int i = 0; i < subtasks.length; i++) {
+            aFutures.add(aReader.simulDFA(subtasks[i]));
+        }
+
+        State prevState = null;
+        State currentState = aDFA.getState(1);
+        // Main body. Read the string char by char and perform the necessary computations
         for (int i = 0; i < aString.length(); i++) {
             prevState = currentState;
             currentState = currentState.readChar(aString.charAt(i));
-            if (currentState == null && prevState.equals(acceptState)) {
+            if (currentState == null && prevState.equals(aDFA.getState(5))) {
                 while (!charQueue.isEmpty()) {
                     finalString.append(charQueue.poll());
                 }
                 finalString.append("_");
-                currentState = this.startState;
-            } else if (currentState == null && !prevState.equals(acceptState)) {
+                currentState = aDFA.getState(1);
+            } else if (currentState == null && !prevState.equals(aDFA.getState(5))) {
                 while (!charQueue.isEmpty()) {
                     charQueue.poll();
                     finalString.append("_");
@@ -146,26 +396,47 @@ class DFAGraph {
                     i--;
                 else 
                     finalString.append("_");
-                currentState = this.startState;
+                currentState = aDFA.getState(1);
             } else {
                 charQueue.add(aString.charAt(i));
             }
         }
         
-        if (currentState != null && currentState.equals(acceptState)) {
+        if (currentState != null && currentState.equals(aDFA.getState(5))) {
             while (!charQueue.isEmpty()) {
                 finalString.append(charQueue.poll());
             }
-        } else if (currentState != null && currentState != acceptState) {
+        } else if (currentState != null && currentState != aDFA.getState(5)) {
             while (!charQueue.isEmpty()) {
                 charQueue.poll();
                 finalString.append("_");
             }
         }
+        // Set the end state
+        endState = prevState;
+        // join them all!
+        for (Future<EncodingType[]> future: aFutures) {
+            try {  
+                EncodingType[] eArr = future.get();
+                for (int i = 0; i < eArr.length; i++) {
+                    if (eArr[i].start.equals(endState)) {
+                        finalString.append(eArr[i].aString.toString());
+                        endState = eArr[i].end;
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getString() {
+        if (aReader != null)
+            aReader.shutdown();
         return finalString.toString();
     }
 }
-
 
 public class q1 {
     /**
@@ -185,12 +456,28 @@ public class q1 {
     public static void main(String [] args) {
         DFAGraph mDFA = new DFAGraph();
         String aString = q1.generateString(100);
-        System.out.println("Running DFA on " + aString);
-        aString = mDFA.runOn(aString);
-        for (int i = 0; i < 5; i++) {
-            State a = mDFA.getState(i+1);
-            System.out.println("State id = " + a.id);
+        try {
+
+            FileWriter fileWriter = new FileWriter("output.txt");
+            // System.out.println("Running DFA on " + aString);
+            fileWriter.write("Running DFA on: " + aString + "\n");
+            System.out.println("Starting executor");
+            NormalRunnable nRun = new NormalRunnable(1, aString, mDFA);
+            Thread t = new Thread(nRun);
+            // Start the task
+            long start = System.currentTimeMillis();
+            t.start();
+            t.join();
+            long totalTime = System.currentTimeMillis()-start;
+            aString = nRun.getString();
+            // System.out.println("OUTPUT: " + aString);
+            fileWriter.write("OUTPUT: " + aString);
+            System.out.println("Time taken: " + totalTime);
+            System.out.println("Length of the output string: " + aString.length());
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace(); 
+            System.out.println("ERROR: you broke.");
         }
-        System.out.println("OUTPUT: " + aString);
     }
 }
